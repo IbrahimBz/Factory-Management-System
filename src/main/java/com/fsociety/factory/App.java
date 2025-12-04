01bbd31b96da4cffd641556a7ab32ba987a921f0
Ibrahim <brhomgamer2@gmail.com>
2025-12-04 13:26:05 +0300

    Removing Quasay code

--- src/main/java/com/fsociety/factory/App.java
+++ src/main/java/com/fsociety/factory/App.java
@@ -4,8 +4,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Heloooooo from qusay");
-        System.out.println("Messi one of the most better player in the world" );
-        System.out.println("qq");
+
     }
 }
