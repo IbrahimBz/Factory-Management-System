f7173ca15f7f069238e338c670abc1ee8ab730d3
Qusay wahhoud <qwahhoud@gmail.com>
2025-12-04 13:04:36 +0300

    Messi

--- src/main/java/com/fsociety/factory/App.java
+++ src/main/java/com/fsociety/factory/App.java
@@ -4,5 +4,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Heloooooo from qusay");}
+        System.out.println("Heloooooo from qusay");
+        System.out.println("Messi one of the most better player in the world" );
+    }
 }
