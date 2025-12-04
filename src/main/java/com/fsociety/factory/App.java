1d06b9b376c2bfebc86851bd38080e8e3f4e4849
Qusay wahhoud <qwahhoud@gmail.com>
2025-12-04 13:00:43 +0300

    This From Amkom Qusay

--- src/main/java/com/fsociety/factory/App.java
+++ src/main/java/com/fsociety/factory/App.java
@@ -1,21 +1,8 @@
 package com.fsociety.factory;
-
-/**
- * Hello world!
- */
-<<<<<<< HEAD
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Heloooooo from qusay");
-=======
-public class App {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-        System.out.println("Hello From Nazeer .......");
->>>>>>> origin/main
-    }
+        System.out.println("Heloooooo from qusay");}
 }
