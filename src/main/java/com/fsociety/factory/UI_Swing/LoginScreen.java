5ae0ac5503e12bdf3deab9d4b303848c002089a4
Ibrahim <brhomgamer2@gmail.com>
2025-12-06 11:04:29 +0300

    Adding UI_Swing Packcage

---
+++ src/main/java/com/fsociety/factory/UI_Swing/LoginScreen.java
@@ -0,0 +1,13 @@
+package com.fsociety.factory.UI_Swing;
+
+
+import javax.swing.*;
+
+public class LoginScreen {
+
+    static void startLogin() {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+
+}
