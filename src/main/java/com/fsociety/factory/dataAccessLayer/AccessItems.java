06cdc5e512efcc4aa36427679e16afd9a55fc83e
Ibrahim <brhomgamer2@gmail.com>
2025-12-06 11:21:13 +0300

    Adding dataAccessLayer Packcage

---
+++ src/main/java/com/fsociety/factory/dataAccessLayer/AccessItems.java
@@ -0,0 +1,16 @@
+package com.fsociety.factory.dataAccessLayer;
+
+public class AccessItems {
+
+    public static boolean createItem() {
+        boolean hasCreated = false;
+
+
+        return hasCreated;
+    }
+
+
+
+
+
+}
