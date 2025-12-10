003e1d8caeae3779b58f35d0da9a14393d9ac571
Ibrahim <115651765+IbrahimBz@users.noreply.github.com>
2025-12-10 17:38:18 +0300

    Update App.java

--- src/main/java/com/fsociety/factory/App.java
+++ src/main/java/com/fsociety/factory/App.java
@@ -6,7 +6,7 @@ package com.fsociety.factory;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        AccessItems.getItems();
 
     }
 }
