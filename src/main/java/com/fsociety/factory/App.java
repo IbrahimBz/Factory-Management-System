68447c6472592271f2686987e1e32f9007bdb95d
Ibrahim <115651765+IbrahimBz@users.noreply.github.com>
2025-12-10 17:41:49 +0300

    Remove 'Hello World' and call AccessItems.getItems()

--- src/main/java/com/fsociety/factory/App.java
+++ src/main/java/com/fsociety/factory/App.java
@@ -6,7 +6,6 @@ package com.fsociety.factory;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-
+        AccessItems.getItems();
     }
 }
